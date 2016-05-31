758c6071563f297a27cca7aeacab8c52fe28cd57
Scotty Carlson <scottythered@users.noreply.github.com>
2016-05-31 14:41:36 -0500

    Update MP3-csv.py

--- MP3-csv.py
+++ MP3-csv.py
@@ -1,5 +1,5 @@
-﻿import csv
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TCOP, TYER, TCON # calls the necessary mutagen library ID3 tags
+import csv
+from mutagen.id3 import ID3, TRCK, TIT2, TALB, TPE1, TCOP, TYER, TCON # calls the necessary mutagen library ID3 tags
 
 csvfile = open('test2.csv') # opens your file, change to suit whatever yours is named
 
@@ -7,8 +7,9 @@ reader = csv.reader(csvfile)
     
 next(reader) # skip header
     
-for songFile, songTitle, album, artist, year, genre, rights in reader: # the columns in your csv
+for trackno, songFile, songTitle, album, artist, year, genre, rights in reader: # the columns in your csv
     audio = ID3(songFile, v2_version=3)
+    audio.add(TRCK(encoding=3, text=trackno))
     audio.add(TIT2(encoding=3, text=songTitle))
     audio.add(TALB(encoding=3, text=album))
     audio.add(TPE1(encoding=3, text=artist))
