7389d0d7a0542db5a361464b4a04e267a5e47613
Scotty Carlson <scottythered@users.noreply.github.com>
2016-05-31 14:09:18 -0500

    Add files via upload

---
+++ MP3-csv.py
@@ -0,0 +1,18 @@
+﻿import csv
+from mutagen.id3 import ID3, TIT2, TALB, TPE1, TCOP, TYER, TCON # calls the necessary mutagen library ID3 tags
+
+csvfile = open('test2.csv') # opens your file, change to suit whatever yours is named
+
+reader = csv.reader(csvfile)
+    
+next(reader) # skip header
+    
+for songFile, songTitle, album, artist, year, genre, rights in reader: # the columns in your csv
+    audio = ID3(songFile, v2_version=3)
+    audio.add(TIT2(encoding=3, text=songTitle))
+    audio.add(TALB(encoding=3, text=album))
+    audio.add(TPE1(encoding=3, text=artist))
+    audio.add(TCOP(encoding=3, text=rights))
+    audio.add(TYER(encoding=3, text=year))
+    audio.add(TCON(encoding=3, text=genre))
+    audio.save(v2_version=3)
