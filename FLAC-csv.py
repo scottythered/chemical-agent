6bded3c6b661e57b20a7648f39058a55e3ac6cbb
Scotty Carlson <scottythered@users.noreply.github.com>
2016-05-31 14:46:41 -0500

    Add files via upload

---
+++ FLAC-csv.py
@@ -0,0 +1,19 @@
+﻿import csv
+from mutagen.flac import FLAC # calls the necessary mutagen library ID3 tags
+
+csvfile = open('test3.csv') # opens your file, change to suit whatever yours is named
+
+reader = csv.reader(csvfile)
+    
+next(reader) # skip header
+    
+for trackno, songFile, songTitle, album, artist, year, genre, rights in reader: # the columns in your csv
+    audio = FLAC(songFile)
+    audio["title"] = songTitle
+    audio["album"] = album
+    audio["artist"] = artist
+    audio["tracknumber"] = trackno
+    audio["copyright"] = rights
+    audio["date"] = year
+    audio["genre"] = genre
+    audio.save()
